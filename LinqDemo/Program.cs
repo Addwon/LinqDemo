33bc9d30eb4343cbdfb4f6eedf489235dd497471
addwon <addwon@gmail.com>
2019-04-26 11:29:02 -0500

    Linq Demo

--- LinqDemo/Program.cs
+++ LinqDemo/Program.cs
@@ -10,6 +10,32 @@ namespace LinqDemo
     {
         static void Main(string[] args)
         {
+            QueryStringArray();
+            Console.ReadLine();
+
         }
+
+        static void QueryStringArray()
+        {
+            string[] dogs = {"K 9", "Brian Griffin",
+            "Scooby Doo", "Old Yeller", "Rin Tin Tin",
+            "Benji", "Charlie B. Barkin", "Lassie",
+            "Snoopy"};
+
+            var dogSpaces = from dog in dogs
+                            where dog.Contains(" ")
+                            orderby dog descending
+                            select dog;
+
+            foreach (var i in dogSpaces)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.WriteLine();
+            
+
+        }
+
     }
 }
